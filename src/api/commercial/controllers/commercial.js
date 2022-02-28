bb304dbc9b527fa887f5b90d537eeb05bca4b557
Alfred Lau <alfred@sulphur.com.sg>
2022-02-28 22:51:31 +0800

    :sparkles: :wrench:

---
+++ src/api/commercial/controllers/commercial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  commercial controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::commercial.commercial');
---
+++ src/api/commercial/routes/commercial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * commercial router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::commercial.commercial');
