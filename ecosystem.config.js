b41072820b1a24d56f37a72720fc937dd394bb6b
Alfred Lau <alfred@sulphur.com.sg>
2022-05-05 12:02:23 +0800

    :wrench: added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "recyclopedia-cms",
+      script: "strapi",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  // Deployment Configuration
+  deploy: {
+    production: {
+      key: "/users/alfredlau/.ssh/recyclopedia",
+      user: "alfred",
+      host: ["192.53.118.42"],
+      ref: "origin/master",
+      repo: "git@gitlab.com:alfredlau/recyclopedia-strapi.git",
+      path: "/var/www/cms",
+    },
+  },
+};
