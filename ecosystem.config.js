061ae7be27567dfff3d18c063bd52ccf9d60b7c6
Alfred Lau <alfred@sulphur.com.sg>
2022-05-05 12:04:52 +0800

    :wrench: updated post-deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,6 +18,7 @@ module.exports = {
       ref: "origin/master",
       repo: "git@gitlab.com:alfredlau/recyclopedia-strapi.git",
       path: "/var/www/cms",
+      "post-deploy": "npx pm2 startOrRestart ecosystem.json --env production",
     },
   },
 };
