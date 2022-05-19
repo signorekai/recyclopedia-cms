e7e365837dd57eff06f59567c213120345f32cad
Alfred Lau <alfred@sulphur.com.sg>
2022-05-20 01:45:20 +0800

    :wrench: added support for `pm2 push production:silent` for no rebuilds

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,5 +22,15 @@ module.exports = {
       "post-deploy":
         "npm run build:prod && pm2 startOrRestart ecosystem.config.js --env production",
     },
+    "production:silent": {
+      key: "/users/alfredlau/.ssh/recyclopedia",
+      user: "alfred",
+      host: ["192.53.118.42"],
+      ref: "origin/master",
+      repo: "git@gitlab.com:alfredlau/recyclopedia-strapi.git",
+      path: "/var/www/cms",
+      "post-setup": "npm run build:prod",
+      "post-deploy": "pm2 startOrRestart ecosystem.config.js --env production",
+    },
   },
 };
