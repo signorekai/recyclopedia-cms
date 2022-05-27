5f4e3e95623eb97e793ee1468555630aaf3cd6b0
Alfred Lau <alfred@sulphur.com.sg>
2022-05-27 15:10:48 +0800

    :fire: removed commercial content type

--- src/api/commercial/controllers/commercial.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  commercial controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::commercial.commercial');
