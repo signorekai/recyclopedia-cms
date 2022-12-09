23510fecd0f268737a5bd89d6157d62e8595ed97
Alfred Lau <alfred@sulphur.com.sg>
2022-12-09 18:27:14 +0800

    :wrench: added item tag

---
+++ src/api/item-tag/controllers/item-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * item-tag controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::item-tag.item-tag');
---
+++ src/api/item-tag/services/item-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * item-tag service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::item-tag.item-tag');
