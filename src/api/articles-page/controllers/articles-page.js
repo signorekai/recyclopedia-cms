05525bda48d7d5aab526802162752db33e2a5830
Alfred Lau <alfred@sulphur.com.sg>
2023-03-16 17:20:06 +0800

    :fire: no need for this

--- src/api/articles-page/controllers/articles-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * articles-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::articles-page.articles-page');
--- src/api/articles-page/routes/articles-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * articles-page router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::articles-page.articles-page');
