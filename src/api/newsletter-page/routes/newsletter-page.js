cd98701657e5f1d7979d2d7d9620d66b78a55d29
Alfred Lau <alfred@sulphur.com.sg>
2023-03-16 17:41:28 +0800

    :wrench: newsletter page

---
+++ src/api/newsletter-page/routes/newsletter-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newsletter-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::newsletter-page.newsletter-page');
---
+++ src/api/newsletter-page/services/newsletter-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newsletter-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::newsletter-page.newsletter-page');
