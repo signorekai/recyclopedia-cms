a56b6c3eb3daa3e958f15df44e4ff2d4652847ec
Alfred Lau <alfred@sulphur.com.sg>
2023-03-29 17:59:33 +0800

    :sparkles: added lifecycle check to check if `data` passed has more keys than specified

--- src/api/item/content-types/item/lifecycles.js
+++ src/api/item/content-types/item/lifecycles.js
@@ -56,120 +56,19 @@ const handler = async (event) => {
 };
 
 const beforeUpdateHandler = async (event) => {
-  const { action } = event;
-  let { where, data } = event.params;
+  let { data } = event.params;
 
-  let currentEntry = await strapi.entityService.findOne(
-    "api::item.item",
-    where.id,
-    {
-      populate: "*",
-    }
-  );
-
-  console.log(data);
-
-  const fieldExceptions = [
-    "visits",
-    "contentUpdatedAt",
-    "updatedAt",
-    "SEO",
-    "recommendations",
-    // "title",
-    // "otherInfo",
-    // "alternateSearchTerms",
-    // "slug",
-    // "featured",
-    // "bluebin",
-    // "recycleElsewhere",
-    // "showSimilar",
-    // "images",
-    // "SEO",
-  ];
+  const fieldExceptions = ["visits", "contentUpdatedAt", "updatedAt"];
 
   let entryChanged = false;
 
-  for (const [key, value] of Object.entries(data)) {
-    if (fieldExceptions.indexOf(key) > -1) {
-      console.log("skipping", key);
-      continue;
-    }
-
-    if (currentEntry.hasOwnProperty(key) === false) {
-      console.log(key, "new content in field");
+  for (const key of Object.keys(data)) {
+    if (fieldExceptions.indexOf(key) === -1) {
       entryChanged = true;
-    } else if (typeof currentEntry[key] === "object") {
-      if (Array.isArray(value) === false) {
-        if (value === null || currentEntry[key] === null) {
-          // is `null`
-          if (value !== currentEntry[key]) {
-            console.log(
-              key,
-              "either current entry / updated entry is not null"
-            );
-            entryChanged = true;
-          }
-        } else if (typeof value === "number") {
-          // oneToOne relation
-          if (value !== currentEntry[key].id) {
-            console.log(key, "oneToOne relation changed");
-            entryChanged = true;
-          }
-        } else if (value.id !== currentEntry[key].id) {
-          // is `object`
-          console.log(key, "object is different", value, currentEntry[key]);
-          entryChanged = true;
-        }
-      } else {
-        // is `array` and same length
-        value.every((item, index) => {
-          if (
-            typeof item === "object" &&
-            item.id !== currentEntry[key][index].id
-          ) {
-            // somehow image uploads do return an object
-            console.log(key, "image upload changed");
-            entryChanged = true;
-            return false;
-          } else if (
-            typeof item !== "object" &&
-            (index >= currentEntry[key].length ||
-              item !== currentEntry[key][index].id)
-          ) {
-            console.log(
-              key,
-              "ManyToMany relation / repeatable component changed"
-            );
-            entryChanged = true;
-            return false;
-          }
-          return true;
-        });
-      }
-    } else {
-      entryChanged = value !== currentEntry[key];
-      if (entryChanged) {
-        console.log(key, "not object / array and not the same");
-      }
+      break;
     }
-
-    // entryChanged =
-    //   currentEntry.hasOwnProperty(key) === false ||
-    //   (typeof value === "object" &&
-    //     value !== null &&
-    //     JSON.stringify(value) !== JSON.stringify(currentEntry[key])) ||
-    //   currentEntry[key] !== value;
-
-    // console.log(key, entryChanged);
-
-    // if (typeof value === "object" || entryChanged) {
-    //   console.log(98, key, currentEntry[key], value);
-    // }
-
-    console.log(key, entryChanged);
-
-    if (entryChanged) break;
   }
+
   console.log("entry changed:", entryChanged);
   if (entryChanged) {
     data.contentUpdatedAt = data.updatedAt;
