9ab936a2d6931f3a11a50d448314b0d04b257e82
Alfred Lau <alfred@sulphur.com.sg>
2023-03-30 15:10:18 +0800

    :alembic: added controller for route

--- src/api/item/controllers/item.js
+++ src/api/item/controllers/item.js
@@ -1,9 +1,41 @@
-'use strict';
+"use strict";
 
 /**
  *  item controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::item.item');
+module.exports = createCoreController("api::item.item", ({ strapi }) => ({
+  async updateNoDate(ctx) {
+    const items = await strapi.entityService.findMany("api::item.item", {
+      filters: {
+        contentUpdatedAt: {
+          $null: true,
+        },
+      },
+    });
+
+    // for (const item of items) {
+    const item = items[0];
+    const result = await strapi.entityService.update(
+      "api::item.item",
+      item.id,
+      {
+        data: {
+          contentUpdatedAt: item.updatedAt,
+        },
+      }
+    );
+
+    if (result) {
+      console.log("updated", item.id);
+    } else {
+      console.error("failed", item.id, result);
+    }
+    // }
+
+    ctx.type = "application/json";
+    ctx.body = items;
+  },
+}));
